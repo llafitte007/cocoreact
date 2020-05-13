8d8d30625e4b42277c8a16ac54f03aa9504c64a4
llafitte007 <luclafitte@gmail.com>
2020-05-13 12:12:27 +0200

    set error status code as parameter

--- src/DefaultFormErrorService.ts
+++ src/DefaultFormErrorService.ts
@@ -3,8 +3,16 @@ import { IFormError } from "./core/FormError/IFormError";
 import { IFormErrorService } from "./core/FormError/IFormErrorService";
 
 export default class DefaultFormErrorService implements IFormErrorService {
+	_errorStatus: number;
+
+	constructor(errorStatus: number) {
+		this._errorStatus = errorStatus;
+	}
+
 	isValid(response: any): boolean {
-		return response && response.data && response.status === 417;
+		return (
+			response && response.data && response.status === this._errorStatus
+		);
 	}
 
 	parse(response: any): string | IFormError[] | undefined {
@@ -13,6 +21,8 @@ export default class DefaultFormErrorService implements IFormErrorService {
 			return data as IFormError[];
 		} else if (data.message) {
 			return data.message as string;
+		} else if (data) {
+			return data;
 		}
 		return undefined;
 	}
