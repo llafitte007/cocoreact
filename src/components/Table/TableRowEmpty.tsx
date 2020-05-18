059c27b8e3852cd4bf953b32447041aa6df47dce
llafitte007 <luclafitte@gmail.com>
2020-05-18 18:44:14 +0200

    remove parent tbody (moved in Table)

--- src/components/Table/TableRowEmpty.tsx
+++ src/components/Table/TableRowEmpty.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-	TableBody as MuiTableBody,
-	TableRow,
-	TableCell
-} from "@material-ui/core";
+import { TableRow, TableCell } from "@material-ui/core";
 
 export interface TableRowEmptyProps {
 	colSpan: number;
@@ -15,12 +11,10 @@ export default function TableRowEmpty({
 	noDataLabel
 }: TableRowEmptyProps) {
 	return (
-		<MuiTableBody>
-			<TableRow>
-				<TableCell colSpan={colSpan} scope="row">
-					{noDataLabel}
-				</TableCell>
-			</TableRow>
-		</MuiTableBody>
+		<TableRow>
+			<TableCell colSpan={colSpan} scope="row">
+				{noDataLabel}
+			</TableCell>
+		</TableRow>
 	);
 }
