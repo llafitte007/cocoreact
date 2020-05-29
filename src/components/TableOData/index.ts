5cbbb522d87d4dc6ecd68480e8228de6c1805aa6
llafitte007 <luclafitte@gmail.com>
2020-05-29 11:24:16 +0200

    update exports

--- src/components/TableOData/index.ts
+++ src/components/TableOData/index.ts
@@ -1,5 +1,11 @@
 import TableOData from "./TableOData";
+import TableRowFields from "./TableRowFields";
+import TableRowFilters from "./TableRowFilters";
+import TableRowPagination from "./TableRowPagination";
 
 export * from "./TableOData";
+export * from "./TableRowFields";
+export * from "./TableRowFilters";
+export * from "./TableRowPagination";
 
-export { TableOData };
+export { TableOData, TableRowFields, TableRowFilters, TableRowPagination };
