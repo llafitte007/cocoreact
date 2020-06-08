66e3ae8a84d3487d9eb9f29b9491680ec0d1c1c5
llafitte007 <luclafitte@gmail.com>
2020-06-08 19:54:22 +0200

    remove default form, table, table odata & json serializer

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,15 @@
 import AxiosHttpClient from "./AxiosHttpClient";
 import DefaultFormErrorService from "./DefaultFormErrorService";
-import DefaultFormFieldOptionsBuilder from "./DefaultFormFieldOptionsBuilder";
-import DefaultTableFieldOptionsBuilder from "./DefaultTableFieldOptionsBuilder";
-import DefaultTableODataFieldOptionsBuilder from "./DefaultTableODataFieldOptionsBuilder";
 import JsonSerializer from "./JsonSerializer";
 import JwtHttpClientDecorator from "./JwtHttpClientDecorator";
 import ThemeLocalStorage from "./ThemeLocalStorage";
 
-import defaultJsonSerializer from "./defaultJsonSerializer";
-import defaultFormWidgetOptionsBuilder from "./defaultFormWidgetOptionsBuilder";
-import defaultTableWidgetOptionsBuilder from "./defaultTableWidgetOptionsBuilder";
-
 export * from "./AxiosHttpClient";
 export * from "./DefaultFormErrorService";
-export * from "./DefaultFormFieldOptionsBuilder";
-export * from "./DefaultTableFieldOptionsBuilder";
-export * from "./DefaultTableODataFieldOptionsBuilder";
 export * from "./JsonSerializer";
 export * from "./JwtHttpClientDecorator";
 export * from "./ThemeLocalStorage";
 export * from "./StringExtension";
-
-export * from "./defaultJsonSerializer";
-export * from "./defaultFormWidgetOptionsBuilder";
-export * from "./defaultTableWidgetOptionsBuilder";
 
 export * from "./components";
 export * from "./core";
@@ -31,13 +17,7 @@ export * from "./core";
 export {
 	AxiosHttpClient,
 	DefaultFormErrorService,
-	DefaultFormFieldOptionsBuilder,
-	DefaultTableFieldOptionsBuilder,
-	DefaultTableODataFieldOptionsBuilder,
 	JsonSerializer,
 	JwtHttpClientDecorator,
-	ThemeLocalStorage,
-	defaultJsonSerializer,
-	defaultFormWidgetOptionsBuilder,
-	defaultTableWidgetOptionsBuilder
+	ThemeLocalStorage
 };
