8673f2b5165fad3979441d0f784b6b8b01cc4dc8
llafitte007 <luclafitte@gmail.com>
2020-06-09 18:10:22 +0200

    add forgotten export

--- src/core/TableField/index.tsx
+++ src/core/TableField/index.tsx
@@ -1,6 +1,8 @@
 import TableFieldOptionsBuilder from "./TableFieldOptionsBuilder";
+import TableODataFieldOptionsBuilder from "./TableODataFieldOptionsBuilder";
 
 export * from "./ITableField";
 export * from "./TableFieldOptionsBuilder";
+export * from "./TableODataFieldOptionsBuilder";
 
-export { TableFieldOptionsBuilder };
+export { TableFieldOptionsBuilder, TableODataFieldOptionsBuilder };
