cd317c8445be0ae04e765c7f31da2f5b438a250d
llafitte007 <luclafitte@gmail.com>
2021-03-26 16:52:20 +0100

    set IAuthService as generic with payload

--- src/components/Auth/AuthContextProvider.tsx
+++ src/components/Auth/AuthContextProvider.tsx
@@ -3,21 +3,21 @@ import React, { useState, useEffect, useCallback, useMemo } from "react";
 
 import AuthContext from "./AuthContext";
 
-export interface IAuthService {
+export interface IAuthService<TPayload = any> {
 	getToken: () => string | null;
 	setToken: (token: string | null) => void;
-	getPayload<T>(token: string): T;
+	getPayload(token: string): TPayload;
 }
 
-export interface AuthContextProviderProps {
-	children?: React.ReactNode;
-	authService: IAuthService;
+export interface AuthContextProviderProps<TPayload = any> {
+	children?: React.ReactNode | React.ReactNode[];
+	authService: IAuthService<TPayload>;
 }
 
-export default function AuthContextProvider<TPayload>({
+export default function AuthContextProvider<TPayload = any>({
 	children,
 	authService
-}: AuthContextProviderProps) {
+}: AuthContextProviderProps<TPayload>) {
 	const [mounted, setMounted] = useState(false);
 	const [logged, setLogged] = useState(false);
 	const [token, setToken] = useState<string | null>(null);
